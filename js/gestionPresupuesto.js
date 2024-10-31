45debedce4a7d555e4c6ba65e3499fd6b03f9745
Celia Garcia <garciafustercelia@gmail.com>
2024-10-31 21:22:32 +0100

    calcularTotalGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -75,6 +75,14 @@ function borrarGasto(id){
     }
 }
 
+function calcularTotalGastos(){
+    let totalGastos = 0;
+    for (let gasto of gastos){
+        totalGastos = gasto.valor + totalGastos;
+    }
+    return totalGastos;
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
