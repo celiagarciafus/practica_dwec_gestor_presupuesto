7bf74071fbf6241c8e2a8d50f176345b9a7fad10
Celia Garcia <garciafustercelia@gmail.com>
2024-10-31 21:26:44 +0100

    borrado enunciado

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -64,7 +64,7 @@ Etiquetas:\n`
             }
         }
     }
-    
+
     this.descripcion = descripcion;
     if (valor>=0){
         this.valor = valor;
@@ -124,9 +124,6 @@ function calcularBalance(){
     return balance;
 }
 
-// NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
-// Las funciones y objetos deben tener los nombres que se indican en el enunciado
-// Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {  
     listarGastos,
     anyadirGasto,
